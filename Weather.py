06b624a3fa42850c043d95f0af34f9f0e98040c5
Abhishek Kumar <abhi.kumar0799@gmail.com>
2023-04-17 09:56:20 +0530

    Initial Commit

---
+++ Weather.py
@@ -0,0 +1,107 @@
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from tkinter import ttk,messagebox
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests
+import pytz
+
+root=Tk()
+root.title("Weather App")
+root.geometry("900x500+300+200")
+root.resizable(False, False)
+
+def getweather():
+    try:
+        city=textfield.get()
+        
+        geolocator=Nominatim(user_agent="geoapiExercises")
+        location=geolocator.geocode(city)
+        obj=TimezoneFinder()
+        result = obj.timezone_at(lng=location.longitude, lat=location.latitude)
+
+        home=pytz.timezone(result)
+        local_time=datetime.now(home)
+        current_time=local_time.strftime("%I:%M %p")
+        clock.config(text=current_time)
+        name.config(text="CURRENT WEATHER")
+
+        #weather
+        api="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=fe952f9b69a7b12dd4e42d3f2a55f6f4"
+        json_data = requests.get(api).json()
+        condition = json_data['weather'][0]['main']
+        description = json_data['weather'][0]['description']
+        temp = int(json_data['main']['temp']-273.15)
+        humidity = json_data['main']['humidity']
+        pressure = json_data['main']['pressure']
+        wind = json_data['wind']['speed']
+
+        t.config(text=(temp,"°"))
+        c.config(text=(condition,"|","FEELS","LIKE",temp,"°"))
+
+        w.config(text=wind)
+        h.config(text=humidity)
+        d.config(text=condition)
+        p.config(text=pressure)
+
+    except Exception as e:
+        messagebox.showerror("Weather App", "Invalid Entry!")
+    
+#Search Bar
+Search_image = PhotoImage(file="search.png")
+myimage=Label(image=Search_image)
+myimage.place(x=20,y=20)
+
+textfield =tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),bg="#404040",border=0,fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
+
+search_icon=PhotoImage(file="search_icon.png")
+myimage_icon=Button(image=search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getweather)
+myimage_icon.place(x=400,y=34)
+
+#Logo
+Logo_image=PhotoImage(file="logo.png")
+logo = Label(image=Logo_image)
+logo.place(x=150,y=100)
+
+#Bottom Box
+Frame_image = PhotoImage(file="box.png")
+frame_myimage = Label(image=Frame_image)
+frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
+
+#time
+name= Label(root, font=("arial",15,"bold"))
+name.place(x=30,y=100)
+clock=Label(root,font=("Helvetica",20))
+clock.place(x=30,y=130)
+
+#label
+label1=Label(root,text="WIND",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label1.place(x=120,y=400)
+
+label2=Label(root,text="HUMIDITY",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label2.place(x=250,y=400)
+
+label3=Label(root,text="DESCRIPTION",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label3.place(x=430,y=400)
+
+label4=Label(root,text="PRESSURE",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label4.place(x=650,y=400)
+
+t=Label(font=("arial",70,"bold"),fg="#ee666d")
+t.place(x=400,y=150)
+c=Label(font=("arial",15,"bold"))
+c.place(x=400,y=250)
+
+w=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+w.place(x=120,y=425)
+h=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+h.place(x=250,y=425)
+d=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+d.place(x=430,y=425)
+p=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+p.place(x=650,y=425)
+
+root.mainloop()
